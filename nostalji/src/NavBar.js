0b9e8f7323110f59d7224958bc1e7d3bbb6c6135
Galip <galipsvt16@gmail.com>
2021-06-29 23:26:59 +0200

    navbar margin edited

--- nostalji/src/NavBar.js
+++ nostalji/src/NavBar.js
@@ -4,33 +4,43 @@ import { Link } from "react-router-dom";
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Logo from "./Images/logo1.png"
+
+import { CgInstagram } from 'react-icons/cg';
+
+
 
 function NavBar() {
   return (
     <div className="navBar" >
 
       <Container bg="dark">
-      <h1>Logo</h1>
+        <Nav.Link > <Link to="/home"  >  <img src={Logo} alt="sekerleme" width="20%" /></Link></Nav.Link>
         <h1>Nostalji Sekerleme</h1>
         <p>Organizasyonunuza Tat Katiyoruz..</p>
       </Container>
 
-    
-        <Navbar bg="light"  expand="md" >
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="m-2">
-            <Nav className="mr-2" >
-              <Nav.Link to="/home" > <Link  > Anasayfa</Link></Nav.Link>
-              <Nav.Link ><Link to="/about" > Hakkimizda</Link></Nav.Link>
-              <Nav.Link > <Link to="/products" > Ürünler</Link></Nav.Link>
-              <Nav.Link > <Link to="/gallery" > Galeri</Link></Nav.Link>
-              <Nav.Link > <Link to="/events" > Etkinlikler</Link></Nav.Link>
-              <Nav.Link > <Link to="/instagram" > Instagram</Link></Nav.Link>
-              <Nav.Link > <Link to="/contact" > Iletisim</Link></Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-  
+
+      <Navbar bg="light" expand="md" >
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" >
+          <Nav className="mr-auto mb-0" >
+            <Nav.Link > <Link to="/home" > Anasayfa</Link></Nav.Link>
+            <Nav.Link ><Link to="/about" > Hakkimizda</Link></Nav.Link>
+            <Nav.Link > <Link to="/products" > Ürünler</Link></Nav.Link>
+            <Nav.Link > <Link to="/gallery" > Galeri</Link></Nav.Link>
+            <Nav.Link > <Link to="/events" > Etkinlikler</Link></Nav.Link>
+            <Nav.Link > <Link to="/instagram" > Instagram</Link></Nav.Link>
+            <Nav.Link > <Link to="/contact" > Iletisim</Link></Nav.Link>
+          </Nav>
+
+         <span>0537 637 06 81</span>
+
+        </Navbar.Collapse>
+
+      </Navbar>
+
 
 
 
