69212d856a42c3b155ba7c43c5084a9e5d2ba565
Riginana <rigina.rysbekova@gmail.com>
2021-07-09 10:57:22 +0200

    deleted h1

--- nostalji/src/Pages/Home.jsx
+++ nostalji/src/Pages/Home.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
-    return (
-        <div style={{height:"71vh", background : "red"}} >
-         Home
-        </div>
-    )
+  return <div style={{ height: "71vh", background: "red" }}></div>;
 }
 
-export default Home
+export default Home;
