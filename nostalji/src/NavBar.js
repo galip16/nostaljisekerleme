0130898ba11b7c2b230222f196499c5ef3ad56d1
Galip <galipsvt16@gmail.com>
2021-07-09 10:22:13 +0200

    copy

--- nostalji/src/NavBar.js
+++ nostalji/src/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
-import { Navbar, Nav, Container,Button } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Logo from "./Images/logo1.png"
@@ -14,36 +14,40 @@ function NavBar() {
   return (
     <div className="navBar" >
 
-      <Container bg="dark">
+      <div>
         <Nav.Link > <Link to="/"  >  <img src={Logo} alt="sekerleme" width="20%" /></Link></Nav.Link>
         <h1>Nostalji Sekerleme</h1>
         <p>Organizasyonunuza Tat Katiyoruz..</p>
-      </Container>
+      </div>
+
+
 
 
       <Navbar bg="light" expand="md" >
+        <Container fluid bg="light">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" >
+            <Nav className="mr-auto" >
+              <Nav.Link > <Link to="/" > Anasayfa </Link></Nav.Link>
+              <Nav.Link > <Link to="/about" > Hakkimizda </Link></Nav.Link>
+              <Nav.Link > <Link to="/products" > Ürünler </Link></Nav.Link>
+              <Nav.Link > <Link to="/gallery" > Galeri </Link></Nav.Link>
+              <Nav.Link > <Link to="/events" > Etkinlikler </Link></Nav.Link>
+              <Nav.Link > <Link to="/instagram" > Instagram </Link></Nav.Link>
+              <Nav.Link > <Link to="/contact" > Iletisim </Link></Nav.Link>
+            </Nav>
 
-        <Navbar.Collapse id="basic-navbar-nav" >
-          <Nav className="mr-auto mb-0" >
-            <Nav.Link > <Link to="/" > Anasayfa </Link></Nav.Link>
-            <Nav.Link > <Link to="/about" > Hakkimizda </Link></Nav.Link>
-            <Nav.Link > <Link to="/products" > Ürünler </Link></Nav.Link>
-            <Nav.Link > <Link to="/gallery" > Galeri </Link></Nav.Link>
-            <Nav.Link > <Link to="/events" > Etkinlikler </Link></Nav.Link>
-            <Nav.Link > <Link to="/instagram" > Instagram </Link></Nav.Link>
-            <Nav.Link > <Link to="/contact" > Iletisim </Link></Nav.Link>
-          </Nav>
+            <Nav>
+              <Nav.Link target="_blank" href="https://www.instagram.com/bursa_nostalji_sekerleme/" >  <CgInstagram />  </Nav.Link>
+              <Nav.Link>  <CgFacebook /> </Nav.Link>
 
-          <Nav>
-            <i href="www.instagram.com">  <CgInstagram /> </i>
-            <i href="www.facebook.com">   <CgFacebook /> </i>
-            <Button variant="secondary">0537 637 06 81</Button>
-          </Nav>
 
-        </Navbar.Collapse>
+              <Button variant="secondary">0537 637 06 81</Button>
+            </Nav>
 
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
 
 
