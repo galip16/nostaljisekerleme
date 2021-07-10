cc837f2d228c205d01edcd210ab55bbeebd95bf3
yykoca <yykoca.08@gmail.com>
2021-07-10 14:02:26 +0200

    isMobile func added

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,59 +1,93 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import "./NavBar.css";
-import { Link } from "react-router-dom";
-import { Navbar, Nav, Container, Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import Logo from "./Images/logo1.png"
+import Logo from "./Images/logo1.png";
 
-import { CgInstagram, CgFacebook } from 'react-icons/cg';
-
-
+import { CgInstagram, CgFacebook } from "react-icons/cg";
+import { FiPhoneCall } from "react-icons/fi";
 
 function NavBar() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  //choose the screen size
+  const handleResize = () => {
+    if (window.innerWidth > 720) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  // create an event listener
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  });
   return (
-    <div className="navBar" >
+    // <div className="navBar pt-5">
+    <>
+      <Navbar bg="light">
+        {/* <Container className={`mt-4`}> */}
+        <Container>
+          <Navbar.Brand href="#home" className="d-flex align-items-center">
+            <img
+              alt="Nostalji Sekerleme Logo"
+              src={Logo}
+              width="120"
+              height="80"
+              className="d-inline-block align-center"
+            />
+            <div className="d-flex flex-column ml-3 mt-2">
+              <h4>Nostalji Şekerleme</h4>
+              <p style={{ fontSize: "12px" }}>
+                Organizasyonunuza tat katıyoruz...
+              </p>
+            </div>
+          </Navbar.Brand>
 
-      <div>
-        <Nav.Link > <Link to="/"  >  <img src={Logo} alt="sekerleme" width="20%" /></Link></Nav.Link>
-        <h1>Nostalji Sekerleme</h1>
-        <p>Organizasyonunuza Tat Katiyoruz..</p>
-      </div>
-
-
-
-
-      <Navbar bg="light" expand="md" >
-        <Container fluid bg="light">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-          <Navbar.Collapse id="basic-navbar-nav" >
-            <Nav className="mr-auto" >
-              <Nav.Link > <Link to="/" > Anasayfa </Link></Nav.Link>
-              <Nav.Link > <Link to="/about" > Hakkimizda </Link></Nav.Link>
-              <Nav.Link > <Link to="/products" > Ürünler </Link></Nav.Link>
-              <Nav.Link > <Link to="/gallery" > Galeri </Link></Nav.Link>
-              <Nav.Link > <Link to="/events" > Etkinlikler </Link></Nav.Link>
-              <Nav.Link > <Link to="/instagram" > Instagram </Link></Nav.Link>
-              <Nav.Link > <Link to="/contact" > Iletisim </Link></Nav.Link>
-            </Nav>
-
-            <Nav>
-              <Nav.Link target="_blank" href="https://www.instagram.com/bursa_nostalji_sekerleme/" >  <CgInstagram />  </Nav.Link>
-              <Nav.Link>  <CgFacebook /> </Nav.Link>
-
-
-              <Button variant="secondary">0537 637 06 81</Button>
-            </Nav>
-
-          </Navbar.Collapse>
+          {isMobile ? (
+            <Button variant="outline-info">Bize Ulaşın</Button>
+          ) : (
+            <FiPhoneCall fontSize="25" />
+          )}
         </Container>
       </Navbar>
 
+      <Navbar bg="light" expand="md" variant="light">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto" variant="dark">
+              <Nav.Link href="/">Anasayfa</Nav.Link>
+              <Nav.Link href="/about">Hakkımızda</Nav.Link>
+              <Nav.Link href="/products">Ürünler</Nav.Link>
+              <Nav.Link href="/gallery">Galeri</Nav.Link>
+              <Nav.Link href="/events"> Etkinlikler</Nav.Link>
+              <Nav.Link href="/instagram">Instagram</Nav.Link>
+              <Nav.Link href="/contact">İletişim</Nav.Link>
+            </Nav>
 
-
-    </div>
+            <Nav>
+              <Nav.Link
+                href="https://www.instagram.com/bursa_nostalji_sekerleme/"
+                target="_blank"
+              >
+                <CgInstagram />{" "}
+              </Nav.Link>
+              <Nav.Link
+                href="https://www.instagram.com/bursa_nostalji_sekerleme/"
+                target="_blank"
+              >
+                <CgFacebook />{" "}
+              </Nav.Link>
+              {/* <Button variant="secondary">0537 637 06 81</Button> */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
